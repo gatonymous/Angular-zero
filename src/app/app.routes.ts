0b5449236b9c0e7c703447068331c607debc7e07
gatonymous <124922467+gatonymous@users.noreply.github.com>
2024-07-09 18:55:17 -0500

    star Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+// import routes home and labs
+import { HomeComponent } from './pages/home/home.component';
+import { LabsComponent } from './pages/labs/labs.component';
+
+export const routes: Routes = [
+    {
+        //path: 'home',
+        path: '', // principal page
+        component: HomeComponent
+    },
+    {
+        path: 'labs',
+        component: LabsComponent
+    }
+]
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
-
-
 @Component({
-  selector: 'app-root',
+  selector: 'app-labs',
   standalone: true,
   imports: [RouterOutlet, CommonModule], // Add CommonModule here
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  //imports: [],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
 })
-export class AppComponent {
+export class LabsComponent {
   welcome = 'Hello World!';
   tasks = [
     'instalar angular cli',
